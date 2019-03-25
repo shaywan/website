465bf9d7f82221ef9578ff846ecf412cd0a0f41e
shaywan <shaunwhalley90@gmail.com>
2019-03-25 19:32:25 +0000

    Setup barebones gatsby project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+// import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <div>
+
+  </div>
+)
+
+export default IndexPage
