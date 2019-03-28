56d96f824392f77fdbabe903c5be8a6b693e9caa
shaywan <shaunwhalley90@gmail.com>
2019-03-28 22:13:52 +0000

    Remove aside

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,8 +20,7 @@ const Layout = ({ children }) => {
   const renderLayout = (data) => (
     <div className="wrapper">
       <Header siteTitle={data.site.siteMetadata.title} />
-      <main style={{ maxWidth: `800px`, margin: '0 auto', padding: '40px 20px' }}>{children}</main>
-      <aside></aside>
+      <main>{children}</main>
     </div>
   );
 
