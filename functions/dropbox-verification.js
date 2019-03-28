dedeb460888ecae76f1f76d9086ae9fbcf7c6c36
shaywan <shaunwhalley90@gmail.com>
2019-03-28 23:10:32 +0000

    Add dropbox verification lambda function

---
+++ functions/dropbox-verification.js
@@ -0,0 +1,14 @@
+exports.handler = function (event, context, callback) {
+  const { queryStringParameters: query } = event;
+
+  if (query.challenge) {
+    callback(null, {
+      statusCode: 200,
+      body: query.challenge,
+      headers: {
+        "Content-Type": "text/plain",
+        "X-Content-Type-Options": "nosniff"
+      }
+    });
+  };
+}
