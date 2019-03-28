84c9ca9774f812bf3f0c432916747e6ff2ad6678
shaywan <shaunwhalley90@gmail.com>
2019-03-28 23:16:44 +0000

    Add error handling to lambda function

--- functions/dropbox-verification.js
+++ functions/dropbox-verification.js
@@ -1,6 +1,12 @@
 exports.handler = function (event, context, callback) {
   const { queryStringParameters: query } = event;
 
+  console.log(JSON.stringify(event, null, 2));
+
+  if (!headers["x-dropbox-signature"]) {
+    callback(new Error('Request not from Dropbox'));
+  }
+
   if (query.challenge) {
     callback(null, {
       statusCode: 200,
