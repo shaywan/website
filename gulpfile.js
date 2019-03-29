be1240106e82b26b2eef4017b77e40d58493015e
shaywan <shaunwhalley90@gmail.com>
2019-03-29 23:18:19 +0000

    Add initial fetchPosts from dropbox

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,19 @@
-const axios = require('axios');
-const fs = require('fs');
 const gulp  = require('gulp');
 const shell = require('gulp-shell');
 
-const getData = () => {
-  console.log(`INCOMING_HOOK_BODY: ${process.env.INCOMING_HOOK_BODY}`);
+const { fetchBlogPosts } = require('./src/services/dropbox');
 
-  if (!process.env.INCOMING_HOOK_BODY) return Promise.resolve();
+const fetchPosts = () => {
+  const webhookBody = process.env.INCOMING_HOOK_BODY;
 
-  const endpoint = 'https://api.chucknorris.io/jokes/random';
+  console.log(`INCOMING_HOOK_BODY: ${webhookBody}`);
 
+  if (!webhookBody) return Promise.resolve();
 
-  return axios.get(endpoint)
-    .then(response => {
-      fs.writeFile('src/data/joke.json', JSON.stringify(response.data), (error) => {
-        if (error) console.log(error);
-
-        console.log('API Success: File saved');
-      })
-    })
-    .catch(error => {
-      console.log(`API Error: ${error}`);
-    });
+  return fetchBlogPosts();
 };
 
-gulp.task('get-data', getData);
+gulp.task('fetch-posts', fetchPosts);
 gulp.task('gatsby', shell.task('gatsby build'))
 
-gulp.task('build', gulp.series('get-data', 'gatsby'));
+gulp.task('build', gulp.series('fetch-posts', 'gatsby'));
---
+++ src/services/dropbox.js
@@ -0,0 +1,25 @@
+const fs = require("fs-extra");
+const path = require('path');
+
+const { Dropbox } = require("dropbox");
+
+const fetchBlogPosts = () => {
+  const dbx = new Dropbox({
+    accessToken: process.env.ACCESS_TOKEN_DROPBOX
+  });
+
+  // Remove and re-create the posts directory
+  // const POSTS_DIR = path.resolve(__dirname, '../pages/posts');
+  // fs.removeSync(POSTS_DIR);
+  // fs.ensureDirSync(POSTS_DIR);
+
+  return dbx
+    .filesListFolder({ path: '' })
+    .then(response => {
+      console.log(response);
+    });
+}
+
+module.exports = {
+  fetchBlogPosts
+}
